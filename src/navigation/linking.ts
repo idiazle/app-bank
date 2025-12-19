fd79bbd2ec3cee51d7ad630b35ba9658f1d93036
Ivan Diaz <ivan.diaz.leuccalla@gmail.com>
2025-12-19 09:44:44 -0500

    fix: modify

--- src/navigation/linking.ts
+++ src/navigation/linking.ts
@@ -2,7 +2,19 @@ export const linking = {
   prefixes: ['appbank://'],
   config: {
     screens: {
+      Login: 'login',
+      Loading: 'loading',
+      CoreBottomTab: {
+        path: 'tabs',
+        screens: {
+          Home: 'home',
+          Chat: 'chat',
           Configuration: 'configuration',
+        },
+      },
+      Main: 'main',
+      Tarjet: 'tarjet',
+      Movements: 'movements',
     },
   },
 };
